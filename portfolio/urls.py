238e869343c332be7d0a1f61968c60d4ec75cee4
yamil-udemy <yamil.muza2@gmail.com>
2019-10-15 12:44:29 -0300

    change in urs.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,5 +21,6 @@ import jobs.views   #importa las views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',jobs.views.home, name='home'),#ir a la app 'jobs',dentro ir al archivo 'views', y dentro del archivo a la funcion 'home'
+    path('',jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),#ir a la app 'jobs',dentro ir al archivo 'views', y dentro del archivo a la funcion 'home'
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
